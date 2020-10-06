8c02e19d07c31a2ed7a52b33f996728ae96877f9
Alex Kreidler <alexkreidler2020@gmail.com>
2020-10-06 01:08:06 -0400

    feat: rewrote to use more native alcaeus types, reorg

--- src/components/DocList.tsx
+++ src/components/DocList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DEFAULT_ENTRYPOINT, getDocs } from "../services/get";
+import { DEFAULT_ENTRYPOINT, getDocs, getEntities } from "../services/get";
 import { useAsync } from "react-async-hook";
 import { Card } from "@blueprintjs/core";
 import { IDNode } from "@semanticweb/loqu";
@@ -7,30 +7,20 @@ import { IDNode } from "@semanticweb/loqu";
 export const DocList = () => {
   //   let d = getDocs(examples[0]);
   //   console.log(d);
-  const status = useAsync(getDocs, [DEFAULT_ENTRYPOINT]);
+  const status = useAsync(getEntities, [DEFAULT_ENTRYPOINT]);
   if (status.loading) return <>Loading...</>;
   if (status.error) throw status.error;
   // return <>{`Something went wrong: ${status.error.message}`}</>;
   if (status.result) {
-    console.log(status.result);
+    // console.log(status.result);
 
     return (
       <div>
-        {status.result.doc.supportedClass
-          .filter((c: IDNode) => !c["@id"].includes("hydra"))
-          .map((c: any) => (
-            <Card key={c["@id"]}>
-              {c.title} - {c.description}
-            </Card>
-          ))}
-        {/* {status.result.doc.supportedClasses
-          .toArray()
-          .filter((c) => !c.iri.includes("hydra"))
-          .map((c) => (
-            <Card key={c.iri}>
-              {c.displayName} - {c.description}
-            </Card>
-          ))} */}
+        {status.result.map((c) => (
+          <Card key={c.id.value}>
+            {c.title} - {c.description}
+          </Card>
+        ))}
       </div>
     );
   }
--- src/services/get.ts
+++ src/services/get.ts
@@ -1,7 +1,18 @@
-import { Hydra } from "@alexkreidler/alcaeus"; // (or 'alcaeus/node')
+import {
+  ApiDocumentation,
+  Class,
+  DocumentedResource,
+  Hydra,
+} from "@alexkreidler/alcaeus"; // (or 'alcaeus/node')
 import { toJSON } from "@semanticweb/loqu";
 
+import * as hydra from "@rdfine/hydra";
+
 export const DEFAULT_ENTRYPOINT = "http://localhost:9090/hydraRoot/";
+
+const defaultContext = {
+  "@context": "http://localhost:9090/context",
+};
 
 export async function getDocs(url: string): Promise<{ doc: any }> {
   console.log("Getting", url);
@@ -12,11 +23,53 @@ export async function getDocs(url: string): Promise<{ doc: any }> {
   // contains supported classes, operations, etc.
   const apiDocs = Hydra.apiDocumentations[0];
   console.log(Hydra.apiDocumentations);
-  const d = apiDocs!.root!;
+  const d = apiDocs!.root! as ApiDocumentation;
+
+  console.log(d);
+
+  // console.log(d.classes);
+
+  const h = (d as unknown) as hydra.ApiDocumentation;
+  console.log(
+    h.supportedClass.map((cls) => cls.title + " DESC: " + cls.description)
+  );
 
   return {
-    doc: await toJSON(d, {
-      "@context": "http://localhost:9090/context",
-    }),
+    doc: await toJSON(d, defaultContext),
   };
+}
+
+export async function getEntities(
+  url: string
+): Promise<(Class & DocumentedResource)[]> {
+  const { response, representation } = await Hydra.loadResource(url);
+  const rootResource = representation!.root!;
+
+  // contains supported classes, operations, etc.
+  const apiDocs = Hydra.apiDocumentations[0];
+  // console.log(Hydra.apiDocumentations);
+  const d = apiDocs!.root! as ApiDocumentation;
+
+  const cs = rootResource.getCollections();
+
+  const outs = await Promise.all(
+    cs.map(async (col) => {
+      console.log(await toJSON(col));
+
+      // We only use the first type from the hydra:collection linked node
+      const tp = Array.from(col.types)[0];
+
+      return tp;
+    })
+  );
+
+  const entities = outs.map(
+    (typ) =>
+      // We only get one type of the same ID from the API documentation
+      d.classes.filter((v) => typ.id.value === v.id.value)[0] as Class &
+        DocumentedResource
+  );
+  console.log(entities);
+
+  return entities;
 }
